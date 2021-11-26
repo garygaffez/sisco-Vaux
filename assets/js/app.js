ba67db4e4702bc461521cb35308e9f1c7e47c13a
garygaffez <gary.gaffez@gmail.com>
2021-11-26 14:22:50 +0100

    1er commit

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+// Carrousel header
+var myCarousel = document.querySelector('#carouselSiscoMobile', '#carouselSiscoSm', '#carouselSiscoMd')
+var carousel = new bootstrap.Carousel(carouselSisco, {
+    interval: 6000,
+    wrap: true,
+    pause: 'hover',
+    touch: true
+})
