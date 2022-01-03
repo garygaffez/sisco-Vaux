be3dadaf190075bf84f804af47d33b3106b1b9cc
garygaffez <gary.gaffez@gmail.com>
2022-01-03 09:17:23 +0100

    debut formulaires front

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,21 @@
 // Carrousel header
 var myCarousel = document.querySelector('#carouselSiscoMobile', '#carouselSiscoSm', '#carouselSiscoMd')
-var carousel = new bootstrap.Carousel(carouselSisco, {
+var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 5000,
+    wrap: true,
+    touch: true
+})
+
+var myCarouselXl = document.getElementById('carouselSiscoXl',)
+var carouselXl = new bootstrap.Carousel(myCarouselXl, {
     interval: 6000,
     wrap: true,
-    pause: 'hover',
+    touch: true
+})
+
+var myCarouselLg = document.getElementById('carouselSiscoLg',)
+var carouselLg = new bootstrap.Carousel(myCarouselLg, {
+    interval: 6000,
+    wrap: true,
     touch: true
 })
---
+++ assets/js/formulaire.js
@@ -0,0 +1,2 @@
+const inputName = document.getElementById('Name');
+
